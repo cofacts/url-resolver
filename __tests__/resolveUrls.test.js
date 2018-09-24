fc7db9d0f5f3756dc4cfc152c1de12480f10aa76
MrOrz <johnsonliang7@gmail.com>
2018-09-24 21:42:22 +0800

    Better fallback workflow for readability.js

--- __tests__/resolveUrls.test.js
+++ __tests__/resolveUrls.test.js
@@ -95,6 +95,7 @@ describe('resolveUrls', () => {
           urls: [
             "http://blog.udn.com/watercmd/1066441" # https://github.com/cofacts/url-resolver/issues/2
             "http://pension.president.gov.tw/cp.aspx?n=0710ED8C9356A871" # This page overrides URL and causes error when fetching topImageUrl...
+            "http://ms7.tw/DL/D?k=app_daily_coupon" # This page don't have error, but executor() returns nothing
           ]
         ) {
           url
