b78b4c75ab612d126ddceb4b8aaf0b03f022ab88
changhc <changhc84@gmail.com>
2019-08-17 18:20:40 +0800

    Test browser stats

--- src/lib/__mocks__/scrap.js
+++ src/lib/__mocks__/scrap.js
@@ -7,5 +7,34 @@ const mod = jest
   .fn()
   .mockImplementation(obj => Promise.resolve(getResult(obj.id)));
 
+const PAGE_COUNT = 3;
+
+const pages = () =>
+  Promise.resolve(
+    [...Array(PAGE_COUNT)].map((_, idx) => ({
+      title: () => Promise.resolve(`Page ${idx}`),
+      url: () => `url ${idx}`,
+      metrics: () =>
+        Promise.resolve({
+          timestamp: 123456789,
+          documents: idx,
+          frames: idx,
+          js_event_listeners: idx,
+          nodes: idx,
+          js_heap_used_size: 512384,
+          js_heap_total_size: 1024768,
+        }),
+    }))
+  );
+
+const getBrowserPromise = () =>
+  Promise.resolve({
+    pages,
+    version: () => Promise.resolve('browser version'),
+  });
+
 module.exports = mod;
 mod.getResult = getResult;
+
+mod.getBrowserPromise = getBrowserPromise;
+mod.pageCount = PAGE_COUNT; // Only in this mocked module;
---
+++ src/resolvers/__tests__/browser.test.js
@@ -0,0 +1,32 @@
+jest.mock('../../lib/scrap');
+const { getBrowserStats } = require('../browser');
+const { pageCount } = require('../../lib/scrap');
+// pageCount is only for testing; it doesn't exist in the real module.
+
+describe('browser', () => {
+  it('should get browser stats', done => {
+    const callback = (_, res) => {
+      expect(res).toHaveProperty('version');
+      expect(res).toHaveProperty('pages');
+      expect(res.pageCount).toBe(pageCount);
+      expect(res.pages[0]).toHaveProperty('title');
+      expect(res.pages[0]).toHaveProperty('url');
+      expect(res.pages[0]).toHaveProperty('metrics');
+      const metrics = res.pages[0].metrics;
+      const metricsProperties = [
+        'timestamp',
+        'documents',
+        'frames',
+        'js_event_listeners',
+        'nodes',
+        'js_heap_used_size',
+        'js_heap_total_size',
+      ];
+      metricsProperties.map(property =>
+        expect(metrics).toHaveProperty(property)
+      );
+      done();
+    };
+    getBrowserStats(undefined, callback);
+  });
+});
